401d6993a2023767ffac8591c26f50eefd544160
vishhaldawane <vishalviniing@gmail.com>
2024-09-17 14:02:12 +0530

    AssociationProducesA

--- OOPSProject/src/ProducesATest.java
+++ OOPSProject/src/ProducesATest.java
@@ -1,20 +1,46 @@
 
 public class ProducesATest {
 
+	/* 
+	 * AAKASH - SKY
+	 * 
+	 * AIR
+	 * 
+	 * FIRE
+	 * 
+	 * WATER - flow - never stops on obstacles 
+	 * 
+	 * EARTH - stability <-- COW is Stable 
+	 * 
+	 * MILK <---   GHEE
+	 * 
+	 * 
+	 
+	 TRANSCENDETAL TO YOUR STATE OF CONSCIOUSNESS
+	 
+	*/
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Milk milk = new Milk("Buffalow","White","Thick",2); //GirCow A2 milk
+		Milk milk = new Milk("Cow","OffWhite","Thin",2); //GirCow A2 milk
 	
 		CurdCulture cc = new CurdCulture(50,8); // 50 is in gm, 5 is in million
 	
 		//Curd curd = new Curd(); //traditional or usual approach
 		
-		Curd curd = milk.coagulate(cc); //telling milk to product curd with
+		Curd curd = milk.coagulate(cc); //telling milk to product curd with  | COAGUALTE
 									// the help of curd culture
 		
-		System.out.println("Curd is : "+curd);
+		System.out.println("Curd is : "+curd); // TRANSMUTATION OF THE MILK
 		
+		Butter butter = curd.churn(30);					// CHURNING <-- AIR
+		
+		System.out.println("Butter : "+butter);
+		
+		ClarifiedButter ghee = butter.boil(10);			 // BOIL <-- FIRE
+		
+		System.out.println("Ghee : "+ghee);
 	}
 
 }
@@ -51,7 +77,115 @@ class Curd
 	public void setSourLevel(String sourLevel) {
 		this.sourLevel = sourLevel;
 	}
+	
+	Butter churn(int minutes) {
+		System.out.println("Churning the "+quantity+" KG of curd for "+minutes+" Minutes to produce the Butter....");
+		String butterColor="";
+		
+		if(thickNess.contains("thin"))
+			butterColor="yellowish";
+		else 		if(thickNess.contains("thick"))
+			butterColor="whiteish";
+
+			
+		Butter butter = new Butter(butterColor,(quantity/3));
+		return butter;
+	}
+	
 }
+
+class ClarifiedButter
+{
+	private float quantity;
+	private String type;
+	private String density;
+	
+	public ClarifiedButter(float quantity, String type, String density) {
+		super();
+		this.quantity = quantity;
+		this.type = type;
+		this.density = density;
+	}
+	public float getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(float quantity) {
+		this.quantity = quantity;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDensity() {
+		return density;
+	}
+	public void setDensity(String density) {
+		this.density = density;
+	}
+	
+	@Override
+	public String toString() {
+		return "ClarifiedButter [quantity=" + quantity + ", type=" + type + ", density=" + density + "]";
+	}
+	
+	
+	
+}
+
+class Butter
+{
+	private String color;
+	private float quantity;
+	
+	public Butter(String color, float quantity) {
+		super();
+		this.color = color;
+		this.quantity = quantity;
+	}
+	
+	ClarifiedButter boil(int mnts) {
+		System.out.println("Boiling "+color+" color Butter of quantity "+quantity+" KG to produce GHEE");
+		String gheeColor="";
+		String density="";
+		
+		if(color.contains("yellow")) {
+			gheeColor="GoldenYellow";
+			density="Flowing";
+		}
+		else
+			if(color.contains("white")) {
+				gheeColor="PureWhite";
+				density="Think And Fat";
+
+			}
+				
+		ClarifiedButter ghee = new ClarifiedButter((quantity/2),gheeColor,density);
+		return ghee;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public float getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(float quantity) {
+		this.quantity = quantity;
+	}
+	@Override
+	public String toString() {
+		return "Butter [color=" + color + ", quantity=" + quantity + "]";
+	}
+	
+	
+	
+}
+
 class Milk {
 	private String type; // cow,buffalow, goat
 	private String color; // offwhite, white
@@ -82,9 +216,9 @@ class Milk {
 			
 			String density="";
 			if(type.equalsIgnoreCase("Cow"))
-				density="Thin";
+				density="thin";
 			else if(type.equalsIgnoreCase("Buffalow"))
-				density="Thick";
+				density="thick";
 			
 			float curdQty=0;
 			
