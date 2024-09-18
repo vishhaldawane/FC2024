a180675d78b8199e4b52c75a0a5e495044c1b99a
vishhaldawane <vishalviniing@gmail.com>
2024-09-18 14:06:25 +0530

    KiteTestStatic

--- StaticProject/src/KiteTest.java
+++ StaticProject/src/KiteTest.java
@@ -1,13 +1,39 @@
 
 public class KiteTest {
-	public static void main(String[] args) {
-		Kite k1 = new Kite("Prakash");
-		Kite k2 = new Kite(120,"Green","Asawari");
-		Kite k3 = new Kite("Red");
 	
+	public static void namedStaticBlock() {
+		System.out.println("namedStaticBlock-> SOME THING COMMMON FOR ALL");
+	}
+	
+	public static void main(String[] args) {
+		
+		namedStaticBlock();
+		namedStaticBlock();
+		
+		System.out.println("BEGIN MAIN ");
+
+		namedStaticBlock();
+
+		Kite.showKiteCount();
+
+		
+		namedStaticBlock();
+
+		
+		Kite k1 = new Kite("Prakash");
+		Kite.showKiteCount();
+		
+		Kite k2 = new Kite(120,"Green","Asawari");
+		Kite.printKiteCount();
+		
+		Kite k3 = new Kite("Red");
+		Kite.printKiteCount();
+
 		k1.fly();
 		k2.fly();
 		k3.fly();
+		
+		
 		
 	}
 }
@@ -18,25 +44,42 @@ class Kite
 	String color;
 	String owner;
 	
-	{
+	static int kiteCount=0; //single Copy
+	
+	static void showKiteCount() {
+		System.out.println("TOTAL KITES IN THE SKY : "+kiteCount);
+	}
+	
+	static void printKiteCount() { // u may run this NAMED static block many times
+		System.out.println("TOTAL KITES IN THE SKY : "+kiteCount);
+	}
+	
+	static {//annonymous static block for the ENTIRE Kite class : runs only ONCE
+		System.out.println("\n static block invoked ");
+		System.out.println("SOME DB CONNECTION, some threads, some network initialization.....\n");;
+	}
+	
+	
+	{ //annonymous non-static block called with EACH ctor call per object
 		System.out.println("Is the sky clear to fly the kite? -- any ctor will follow it --");
 		owner="Unknown";
 		color="Unknown";
 		length=-1;
+		++kiteCount;
 	}
 	
 	Kite(int length) {
-		System.out.println("Kite(int) ");
+		System.out.println("Kite(int) \n");
 		this.length = length;
 	}
 	
 	Kite(String color, int length) {
-		System.out.println("Kite(String,int) ");
+		System.out.println("Kite(String,int) \n");
 		this.color=color;	
 	}
 	
 	Kite(String owner) {
-		System.out.println("Kite(String) ");
+		System.out.println("Kite(String) \n");
 		this.owner=owner;
 	}
 	
@@ -44,7 +87,7 @@ class Kite
 	
 	public Kite(int length, String color, String owner) {
 		super();
-		System.out.println("Kite(int,String,String) ");
+		System.out.println("Kite(int,String,String) \n");
 
 		this.length = length;
 		this.color = color;
